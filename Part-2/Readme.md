fc7af0dd351203e1ef8e8c887c35edd65616cc91
thedebug7 <thedebug7@gmail.com>
2025-02-13 00:33:27 +0200

    add part 2

---
+++ Part-2/Readme.md
@@ -0,0 +1,21 @@
+# Link Js
+
+` <script src="app.js"></script>`
+
+# Tamplet Literals
+
+` (`the sum is : ${price}`) `
+
+# Operators
+
+![](image2/Operators.png)
+
+# Comparison Operator
+
+![](image2/Comparison%20Operator.png)
+
+# Conditional Statement.
+
+- if-else
+- nested if-else
+- switch
---
+++ Part-2/app.js
@@ -0,0 +1,24 @@
+// console.log(`Before my statement.`)
+
+// let age = 20;
+
+// if (age >= 18 ){
+//     console.log('You can Vote...')
+// }
+
+// console.log(`After my Statement.`)
+
+alert('Something is wrong...')
+console.error(`This is error msg...`)
+console.warn(`this is awarning message..`)
+
+console.log(`Program Satrting...`)
+let TL = 'green'
+if(TL === 'red'){
+    console.log(`.....Stop.....`)
+}else if(TL === 'yellow'){
+    console.log(`....Slow Down....`)
+}else if (TL === 'green'){
+    console.log(`......Go.....`)
+}
+console.log(`Programing Ending...`)
