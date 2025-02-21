1d8e5a6b08d7baa9170129cb66729d51b0827323
thedebug7 <thedebug7@gmail.com>
2025-02-22 00:25:04 +0200

    add loop

---
+++ Part-4(loop)/Readme.md
@@ -0,0 +1,9 @@
+# Loops
+
+## For Loop
+
+![](image4/for-loop.png)
+
+# Nested for loop
+
+![](image4/Nested%20for%20loop.png)
---
+++ Part-4(loop)/app.js
@@ -0,0 +1,22 @@
+console.log('hello');
+
+// for (let i=1; i<5; i=i+1){
+//     console.log(i);
+// }
+
+// for (i = 1 ; i <= 15 ; i++){
+//     if (!(i%2===0)){
+//         console.log(i)
+//     }
+// }
+
+// for (let i=1; i <=10; i=i+1){
+//     console.log(`5 * ${i} = ${5*i}`)
+// }
+
+
+for (let i = 1; i<=3; i++){
+    for(let j = 1; j<=3; j++){
+        console.log(j)
+    }
+}
