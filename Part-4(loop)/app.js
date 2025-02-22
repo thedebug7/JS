8547afd412ebc87daf86ad1aa3cad7688734c80c
thedebug7 <thedebug7@gmail.com>
2025-02-23 00:55:36 +0200

    add Nested loop

--- Part-4(loop)/app.js
+++ Part-4(loop)/app.js
@@ -16,7 +16,8 @@ console.log('hello');
 
 
 for (let i = 1; i<=3; i++){
+    console.log(`Outer Loop ${i}`)
     for(let j = 1; j<=3; j++){
-        console.log(j)
+        console.log(`Inner Loop ${j}`)
     }
 }
